9c34a668614e9d28864ea39c2a5c15764dde9c3a
Andrew Gartland <agartlan@fhcrc.org>
2020-12-17 12:48:33 -0800

    Adding catcorr to repo

--- fishersapi/__init__.py
+++ fishersapi/__init__.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import, division, print_function
 from .version import __version__
 from .fishersapi import *
 from .fishersapi import _scipy_fishers_vec
+from .catcorr import catcorr
 
 __all__ = ['fishers_vec',
            'fishers_frame',
-           'adjustnonnan']
+           'adjustnonnan',
+           'catcorr']
